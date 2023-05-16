c96fc154f58dc39cbfd565fdd3b103c9937da8a1
zidanefurtado1 <100311249+zidanefurtado1@users.noreply.github.com>
2023-05-17 00:34:04 +0100

    Update usuario.js

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -5,7 +5,7 @@ const db = require('./../db/models');//icluir o arquivo que tem a conecção com
 
 router.get("/usuario",async  (req, res) =>{
     const usuario = await db.usuario.findAll({
-        attribute:["id","name","address","email","password","type_user"],
+        attribute:["id","name","address","email","password","type_user"], //indicar as colunas 
         order:[['id']]
     })
     if (usuario){
@@ -15,6 +15,24 @@ router.get("/usuario",async  (req, res) =>{
     }else{
         return res.status(400).json({
             mensagem : " erro: usuario não  encontrado ",
+           
+        });
+    }
+});
+
+router.get("/usuario/:id",async (req, res) =>{ 
+    const {id} = req.params; //receber o parametro enviado na url
+    const usuario = await db.usuario.findOne({
+        attribute:["id","name","address","email","password","type_user"], //indicar as colunas 
+        where:{id},  //condição qual quel registo deve ser retornado 
+    });
+
+ 
+    if (usuario){
+        return res.json({usuario:usuario.dataValues}); 
+    }else{
+        return res.status(400).json({
+            mensagem : " erro: usuario não  encontrado",
            
         });
     }
@@ -38,6 +56,44 @@ router.post("/usuario", async (req, res) => {
         });
      }); 
 })
+
+
+router.put("/usuario", async(req, res) => {
+    var dados = req.body;
+    await db.usuario.update(dados,{where: {id: dados.id}}).then(()=>{
+       return res.json({
+        mensagem : "usuario editado com sucesso"
+       });
+    }).catch(() => {
+        return res.status(400).json({
+            mensagem : " erro: usuario não  editado com sucesso ",
+           
+        });
+    });
+   
+});
+
+
+
+
+router.delete("/usuario/:id", async(req, res) => {
+    const {id} = req.params;
+     await db.usuario.destroy({
+        where: {id:id},
+     }).then(()=>{
+        return res.json({
+            mensagem : "usuario apagado com sucesso"
+        });
+     }).catch(()=>{
+        return res.status(400).json({
+            mensagem : " erro: usuario não  apagado com sucesso ",
+           
+        });
+     });
+   
+});
+
+
 
 
 module.exports = router;// exportar a intrução que esta dentro da constante router
