225f942b6629b4eaa47d0aeba4b4447fbd223477
zidanefurtado1 <100311249+zidanefurtado1@users.noreply.github.com>
2023-05-18 16:27:01 +0100

    upd

--- app.js
+++ app.js
@@ -3,20 +3,25 @@ const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 const db = require('./db/models'); //testar conecção     
-
-app.use(express.json()); // formato que quero receber 
-
+const swaggerUi = require("swagger-ui-express");
 const usuario = require('./controllers/usuario');
 const hotel = require('./controllers/hotel');
 const quartos = require('./controllers/quartos');
 const reserva = require('./controllers/reserva');
+const YAML = require('js-yaml');
+const swaggerDocument = YAML.load('./docs'); 
 
+
+
+
+app.use(express.json()); // formato que quero receber 
 app.use('/',usuario);
 app.use('/',hotel);
 app.use('/',quartos);
 app.use('/',reserva);
-
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(bodyParser.json());
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando na porta http://localhost:${port}`);
