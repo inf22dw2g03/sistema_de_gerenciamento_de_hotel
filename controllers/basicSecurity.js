af8f88cef2d3c9e672496fd87215ce1b0d64b44f
EtsonDaSilva <100229514+EtsonDaSilva@users.noreply.github.com>
2023-05-23 19:37:50 +0100

    Update basicSecurity.js

--- controllers/basicSecurity.js
+++ controllers/basicSecurity.js
@@ -3,19 +3,19 @@ const basicAuth = require("basic-auth");
 
 
 async function auth(req, res, next) {
-    var user = basicAuth(req);
-    if (!user || !user.name || !user.pass) {
-      res.set("WWW-Authenticate", "Basic realm=Authorization Required");
-      res.sendStatus(401);
-      return;
-    }
-    if (await usersService.authenticate({username: user.name, password: user.pass})) {
-      next();
-    } else {
-      res.set("WWW-Authenticate", "Basic realm=Authorization Required");
-      res.sendStatus(401);
-      return;
-    }
-  };
+  var user = basicAuth(req);
+  if (!user || !user.name || !user.pass) {
+    res.set("WWW-Authenticate", "Basic realm=Authorization Required");
+    res.sendStatus(401);
+    return;
+  }
+  if (await usersService.authenticate({username: user.name, password: user.pass})) {
+    next();
+  } else {
+    res.set("WWW-Authenticate", "Basic realm=Authorization Required");
+    res.sendStatus(401);
+    return;
+  }
+};
   
 module.exports = auth;
